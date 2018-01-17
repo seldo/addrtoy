7d7afc148e12ac681db8ecdd98ccde05635521a1
Laurie Voss <github@seldo.com>
2018-01-16 21:13:51 -0800

    Templating

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-let parse = require('csv-parse')
-let fs = require('fs')
+const express = require('express')
+const parse = require('csv-parse')
+const fs = require('fs')
+const nunjucks = require('nunjucks')
 
-var filename = process.argv[2]
+const app = express()
 
-fs.readFile(filename, 'utf8', function(err, data) {
-  if (err) throw err;
-  parse(data,{columns:true},(er,stuff) => {
-    console.log(stuff)
+// configure the HTTP server and basic templating
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app,
+    noCache: true // nice for dev mode
+})
+
+app.get('/', function(req, res) {
+  // read the data from the file
+  // so the file gets re-read every time you hit the page
+  // inefficient but who cares
+  let filename = process.argv[2]
+  fs.readFile(filename, 'utf8', function(err, data) {
+    if (err) throw err;
+    parse(data,{columns:true},(er,data) => {
+
+      // render a page with the parsed data
+      res.render('index.html',{
+        Object,
+        title: "Woo",
+        data
+      })
+    })
   })
 })
+
+app.listen(3000, () => console.log('Listening on port 3000'))
